854dcaeef3405d98437e784853468cf6bf8241b7
Borja Martin <borjatecnico2014@gmail.com>
2024-03-27 10:24:55 +0100

    inicio_django

---
+++ programa/App_analytic/analytic_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Type(models.TextChoices):
+    GASTOS = "Gastos"
+    INGRESOS = "Ingresos"
+
+class FileJSON(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    name_type = models.CharField(
+        max_length = 10,
+        choice = Type.choices,
+        default = Type.GASTOS
+    )
